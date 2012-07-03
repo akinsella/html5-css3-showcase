f66e6ea152d26d380a66bfcfd9dca4872badff16
Alexis Kinsella <alexis.kinsella@gmail.com>
2012-07-04 00:16:52 +0200

    Initial commit

---
+++ javascript/showcase.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+    onReadyState();
+});
+
+function onReadyState() {
+    (function ($) {
+      initRouter();
+    })($);
+}
+
+function initRouter() {
+
+    // OK
+    var currentView = null;
+
+    // OK
+    var routes = {
+        '/': function() {
+            // Do Nothing
+        },
+        '/css3-transition-opacity': function() {
+            var page = 'css3-transition-opacity';
+            $.ajax({
+                url: 'pages/' + page + '/index.html',
+                cache: false,
+                dataType: 'html',
+                success: function(data) {
+                    var html = $('<html />').html(data);
+                    var content = $(html).find('#content');
+                    $('#detail').html(content);
+                }
+            });
+
+            $('#styles').attr('href', 'pages/' + page + '/styles.css');
+
+            setTimeout(function() { prettyPrint(); }, 100);
+        }
+    };
+
+    // OK
+    Router(routes).configure({
+        on: function(){
+            amplify.store('hacker-hash', location.hash);
+        },
+        notfound: function(){
+            location.hash = '/';
+        }
+    }).init(amplify.store('hacker-hash') || '/');
+
+}
